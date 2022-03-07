9f87033982e919ad50b0918896ef1bb7cbd8bd9a
eelviral <aldaire991@gmail.com>
2022-03-07 01:41:57 -0500

    Initialization of Chess Repository

---
+++ chess.py
@@ -0,0 +1,15 @@
+# Name: Chess Program
+# Author: Eddie Elvira (@eelviral)
+# Github_Page: https://www.github.com/eelviral/
+# Created_On: Monday, March 7, 2022 at 05:51 UTC
+
+import pieces
+import pygame
+
+from pieces.king import King
+
+class Chess:
+    def __init__(self):
+        pass
+if __name__ == '__main__':
+    King()
