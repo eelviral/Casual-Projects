d8e88a3aa48f2fd27af3c24111900be26852f4c1
Eddie Elvira <69776591+BlitzyMango@users.noreply.github.com>
2022-06-03 05:57:15 -0400

    Added documentation and type checking to find_random_move function

--- ai.py
+++ ai.py
@@ -1,7 +1,12 @@
 import random
 
 
-def find_random_move(valid_moves):
+def find_random_move(valid_moves) -> list:
+    """Picks a random move from a list of valid moves
+
+    :param valid_moves: list of legal chess moves that can be played
+    :return: random move
+    """
     return valid_moves[random.randint(0, len(valid_moves) - 1)]
 
 
