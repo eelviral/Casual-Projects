c6427913768b2f88c32ea4a5218832c2c39ba8b8
Eddie Elvira <69776591+BlitzyMango@users.noreply.github.com>
2022-06-03 05:59:26 -0400

    Implemented the best move function using a greedy algorithm to determine the next best move

--- ai.py
+++ ai.py
@@ -1,5 +1,9 @@
 import random
 
+piece_score = {'K': 0, 'Q': 10, 'R': 5, 'B': 3, 'N': 3, 'P': 1}
+CHECKMATE = 1000
+STALEMATE = 0
+
 
 def find_random_move(valid_moves) -> list:
     """Picks a random move from a list of valid moves
@@ -10,5 +14,54 @@ def find_random_move(valid_moves) -> list:
     return valid_moves[random.randint(0, len(valid_moves) - 1)]
 
 
-def find_best_move():
-    return
+def find_best_move(game, valid_moves):
+    """Finds the best move based on material
+
+    :param game: the current chess game
+    :param valid_moves: list of legal chess moves that can be played
+    :return: the best move that can be currently played
+    """
+    turn_multiplier = 1 if game.current_turn.is_white_side else -1
+    max_score = -CHECKMATE
+    best_move = None
+
+    promotion = True if game.promotion else False
+    for player_move in valid_moves:
+        if promotion:
+            game.move_action(player_move[1], player_move[0])
+        else:
+            game.move_action(player_move[0][1], player_move[0][0])
+            game.move_action(player_move[1][1], player_move[1][0])
+
+        if game.is_checkmate():
+            score = CHECKMATE
+        elif game.is_stalemate():
+            score = 0
+        else:
+            score = turn_multiplier * score_material(game.board)
+
+        if score > max_score:
+            max_score = score
+            best_move = player_move
+
+        game.undo_move()
+    return best_move
+
+
+def score_material(board):
+    """Score the board based on material
+
+    :param board: the chess board
+    :return:
+    """
+    score = 0
+    for row in board.boxes:
+        for box in row:
+            if box.piece is None:
+                continue
+
+            if box.piece.is_white:
+                score += piece_score[str(box.piece)[1]]
+            elif not box.piece.is_white:
+                score -= piece_score[str(box.piece)[1]]
+    return score
--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 from game import Game, GameStatus
 from player import Human, Computer
-from ai import find_random_move
+from ai import find_best_move
 import pygame
 
 MAX_FPS = 15
@@ -33,9 +33,24 @@ if __name__ == '__main__':
                 game.mouse_click(event.pos)
             if event.type == pygame.USEREVENT and isinstance(game.current_turn, Computer):
                 legal_moves = game.get_current_legal_moves()
-                computer_move = find_random_move(legal_moves)
-                game.move_action(computer_move[0][1], computer_move[0][0])
-                game.move_action(computer_move[1][1], computer_move[1][0])
+                if len(legal_moves) == 0:
+                    if game.current_turn.is_white_side:
+                        game.status = GameStatus.BLACK_WIN
+                    else:
+                        game.status = GameStatus.WHITE_WIN
+                else:
+                    if game.promotion:
+                        legal_moves = [[spot.x, spot.y] for spot in game.promotion_pieces]
+                        computer_move = find_best_move(game, legal_moves)
+                        game.move_action(computer_move[1], computer_move[0])
+                    else:
+                        computer_move = find_best_move(game, legal_moves)
+                        game.move_action(computer_move[0][1], computer_move[0][0])
+                        game.move_action(computer_move[1][1], computer_move[1][0])
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_z:
+                    game.undo_move()
         game.draw_board()
 
         if game.status != GameStatus.ACTIVE:
